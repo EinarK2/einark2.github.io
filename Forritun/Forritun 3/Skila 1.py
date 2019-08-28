e25d9677fd5b5a57451c537587399a502e4bb3be
EinarK2 <einarkarlp@gmail.com>
2019-08-28 09:35:46 +0000

    ok

--- Forritun/Forritun 3/Skila 1.py
+++ Forritun/Forritun 3/Skila 1.py
@@ -1,5 +1,7 @@
 # Einar Karl Pétursson
 # 23/8/2019
+import csv
+invoices = []
 
 
 class Invoice:
@@ -20,15 +22,29 @@ class Invoice:
         .format(self.partNumber, self.partDescription, self.quantity, self.pricePerItem))
 
 
-def readFile(nafnTxtSkra):  # Les skrána með csv.reader og smíðar invoice object úr  hverri línu fyrir sig.
+def readFile(filename):  # Les skrána með csv.reader og smíðar invoice object úr  hverri línu fyrir sig.
     # try except finally
-    try:
+    '''try:
         skra = open(nafnTxtSkra, "r", encoding="utf-8")  # opnar skrá
         listi = (skra.read().split(';'))  # Les innihald án bila????
         skra.close()  # Lokar skrá
         return listi  # Skilar lista
     except:
         print("Það er ekkert í skránni")
+    '''
+    file = None
+    try:
+        file = open(filename, "r", newline='\r\n', encoding="utf-8")
+        reader = csv.reader(file, delimiter=';')
+        for row in reader:
+            invoice = Invoice(row[0], row[1], row[2], row[3])
+            invoices.append(invoice)
+    except IOError:
+        print('An error occured trying to read the file')
+    finally:
+        print('Finished')
+        if file != None:
+            file.close()
 
 
 def writeFile():  # Skrifar innihald listans invoiceList í skrána invoice.csv
@@ -44,5 +60,6 @@ def printInvoices():  # Prentar(á skjáinn) alla reikninga ásamt heildarupphæ
 
 
 print("Les Skrá")
-listi = readFile('invoice.csv')
-print(listi)
+readFile('invoice.csv')
+for invoice in invoices:
+    invoice.printInvoice()
