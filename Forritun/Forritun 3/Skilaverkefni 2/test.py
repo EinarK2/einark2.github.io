ba2583e7e9301468d8a90b137cbe4087de0d350f
EinarK2 <einarkarlp@gmail.com>
2019-09-11 09:48:49 +0000

    fforr

--- Forritun/Forritun 3/Skilaverkefni 2/test.py
+++ Forritun/Forritun 3/Skilaverkefni 2/test.py
@@ -1,14 +1,26 @@
 # Einar Karl Pétursson
+import csv
+from klasi import Verkalydsfelag
+verkalydsfelag = []
 
 
 def opnaSkra():
-    pass  # Þetta fall á að opna skrána verkalydsfelag.csv. Lesa skrána.
+    with open('verkalydsfelag.csv', 'r', newline='', encoding='utf-8')as file:
+        reader = csv.reader(file, delimiter=';')
+        for row in reader:
+            print(row)
+            hlutur = Verkalydsfelag(row[0], row[1], row[2, row[3]])
+            verkalydsfelag.append(hlutur)
+    # Þetta fall á að opna skrána verkalydsfelag.csv. Lesa skrána.
     # Því næst býr fallið til tilvik(object,hlut) úr hverri línu skráarinnar og setur í lista.
     # Listinn innheldur þá hluti sem gerðir eru úr klasanum Verkalydsfelag.
 
 
 def skrifaSkra():
-    pass  # Skrifar í skrána.
+    file = open('verkalydsfelag.csv', "w", newline='\r\n', encoding="utf-8")
+    for medlimir in verkalydsfelag:
+        line = medlimir.nafn + ";" + medlimir.felagsnumer + ";" + medlimir.laun + ";" + medlimir.kennitala + "\n"
+        file.write(line)
 
 
 def nyrMedlimur():
@@ -41,3 +53,13 @@ def heildarskattar():
 
 def mittFall(): # nefnið fallið einhverju lýsandi nafni
     pass  # frjálst val. Samt eitthvað spennandi og flott
+
+val = ""
+while val != 0:
+    print("valmynd")
+    print("1. \n2. \n 3. \n")
+    val = input("Veldu")
+    if val == "1":
+        pass
+    elif val == "2":
+        pass
