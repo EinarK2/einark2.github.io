ad9f926da9b9fcf9d0f02678f777b01b8c6be340
Einar Karl <einarkarlp@gmail.com>
2019-09-20 19:58:47 +0000

    forr

--- Forritun/Forritun 3/Skilaverkefni 3/forrit.py
+++ Forritun/Forritun 3/Skilaverkefni 3/forrit.py
@@ -11,21 +11,21 @@ print("NEMI")
 print(nemi1)
 print("-"*35)
 
-grunnskolanemi1 = Grunnskolanemi('2609022450', 'Einar Karl Pétursson', 'kk',
-                                 'Álfaheiði 18', '6948820', 'einarkarlp@gmail.com',
-                                 'Pétur Óli Einarsson', 'Álfhólsskóli')
+grunnskolanemi1 = Grunnskolanemi('1234567890', 'Jón Jónsson', 'kk',
+                                 'Hlíðarhjalli 39', '1234567', 'jon@jon.is',
+                                 'Jón Jóhannesson', 'Álfhólsskóli')
 print("GRUNNSKÓLANEMI")
 print(grunnskolanemi1)
 print("-"*35)
 
-framhaldsskolanemi1 = Framhaldsskolanemi('2609022450', 'Einar Karl Pétursson',
-                                         'kk', 'Álfaheiði 18', '6948820',
-                                         'einarkarlp@gmail.com', 'Tölvubraut', 'Nei')
+framhaldsskolanemi1 = Framhaldsskolanemi('0987654321', 'Jónína Jónsdóttir',
+                                         'kvk', 'Skipholt 6', '7654321',
+                                         'jonina@mail.com', 'Tölvubraut', 'Nei')
 print("FRAMHALDSSKÓLANEMI")
 print(framhaldsskolanemi1)
 print('-'*35)
 
-haskolanemi1 = Haskolanemi('2609022450', 'Einar Karl Pétursson', 'kk', 'Álfaheiði 18',
-                           '6948820', 'einarkarlp@gmail.com', 'PHd', 'Já')
+haskolanemi1 = Haskolanemi('2609022450', 'Pétur Karl Einarsson', 'kk', 'Álfaheiði 19',
+                           '6969920', 'peturkarle@gmail.com', 'PHd', 'Já')
 print("HÁSKÓLANEMI")
 print(haskolanemi1)
